f5dfcc5f9097f3af6c0841dda1ed40828f178cd9
chendong <dongchen@secusoft.cc>
2019-06-06 19:29:58 +0800

    【更新信息】增加常量类

---
+++ src/main/java/com/secusoft/web/core/common/Constants.java
@@ -0,0 +1,15 @@
+package com.secusoft.web.core.common;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ * 常量类
+ * @author ChenDong
+ * @company 视在数科
+ * @date 2019年6月6日
+ */
+public class Constants {
+
+    // 返回成功状态码
+    public static final HttpStatus SUCCESS = HttpStatus.OK;
+}
