8858d43b28aa3d0bf228928ef2ba1b546644e82a
cd <71016@192.168.1.108>
2019-06-18 15:56:55 +0800

    【更新信息】更改同步设备为选配

--- src/main/java/com/secusoft/web/task/SyncDeviceTask.java
+++ src/main/java/com/secusoft/web/task/SyncDeviceTask.java
@@ -3,6 +3,7 @@ package com.secusoft.web.task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -22,10 +23,16 @@ public class SyncDeviceTask {
     @Autowired
     private DeviceService deviceService;
     
+    @Value("${ubr.sync.enable:false}")
+    private boolean enable;
+    
     @Scheduled(cron="0 0 3 * * ?")
     public void syncDevice() {
-        log.info("sync device start");
-        deviceService.syncDeviceFromUbr();
-        log.info("sync device end");
+    	if(enable){
+    		// 开启设备同步
+    		log.info("sync device start");
+    		deviceService.syncDeviceFromUbr();
+    		log.info("sync device end");
+    	}
     }
 }
